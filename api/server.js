608806e4d4dc4333317672c13da97a6208a9a7ba
Sadia Gray <sadiagray@Sadias-MBP.home>
2024-01-16 01:02:22 -0600

    added postsRouter set-up

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const postsRouter = require('./posts/posts-router')
 
 const server = express()
 
 server.use(express.json())
+
+server.use('/api/posts', postsRouter)
 
 
 server.use('*', (req, res) => {
