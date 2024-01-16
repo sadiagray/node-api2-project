06ccaa07fd15368dbdd31acf66c2a908cc0252cd
Sadia Gray <sadiagray@Sadias-MBP.home>
2024-01-16 01:40:02 -0600

    completed second mvp test

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,19 +4,34 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     Post.find()
-    .then(found => {
-        res.json(found)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message: "The posts information could not be retrieved",
-            err: err.message,
-            stack: err.stack, 
+        .then(found => {
+            res.json(found)
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                message: "The posts information could not be retrieved",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
 })
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 })
 router.post('/', (req, res) => {
 
